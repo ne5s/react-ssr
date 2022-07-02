4acccb8fbcd43576a072deea870d67dbd829c25f
ne5s <phobos90@naver.com>
2022-07-02 15:16:57 +0900

    react-ssr 실습완료

--- src/index.js
+++ src/index.js
@@ -9,17 +9,17 @@ const App = require("./client/App").default;
 app.use(express.static("public"));
 
 app.get("*", (req, res) => {
-  const app = ReactDOMServer.renderToString(<App />);
+  const html = ReactDOMServer.renderToString(<App />);
 
   const template = `
   <html>
   <head>
-  <title>SSR React App</title>
+    <title>SSR React App</title>
   </head>
     <body>
 
-    <div id="root">${app}</div>
-    <script src="bundle.js"></script>
+      <div id="root">${html}</div>
+      <script src="bundle.js"></script>
     </body>
   </html>
   `;
